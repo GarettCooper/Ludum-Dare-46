60ac06527142b8c5979a3d2283c38e28dc60bcb3
Garett Cooper <garettcooper@ymail.com>
2020-04-18 19:13:10 -0400

    Polished egg weight and jump height

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    [SerializeField] private Transform targetPosition;
+    [SerializeField] private float cameraMovementSpeed;
+
+    // Update is called once per frame
+    void Update() {
+        transform.Translate((targetPosition.position - transform.position) * cameraMovementSpeed * Time.deltaTime);
+    }
+}
