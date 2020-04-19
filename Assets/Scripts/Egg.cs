7c7694b78e677719d500ca13e1101db42a0c24fe
Garett Cooper <garettcooper@ymail.com>
2020-04-19 11:07:57 -0400

    Added stubs for win and lose conditions

---
+++ Assets/Scripts/Egg.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Egg : MonoBehaviour {
+
+    [SerializeField] private LayerMask loseCollisionMask;
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (loseCollisionMask == (loseCollisionMask | (1 << collision.gameObject.layer))) {
+            Debug.Log("Egg Cracked!");
+            // TODO: Trigger lose scenario
+        }
+    }
+
+
+
+}
---
+++ Assets/Scripts/Nest.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nest : MonoBehaviour {
+
+    [SerializeField]
+    private float eggTime = 2.0f;
+
+    private bool eggPresent;
+
+    private void Update() {
+        if (eggPresent) {
+            Debug.Log("Egg Present");
+            eggTime -= Time.deltaTime;
+        }
+
+        if (eggTime <= 0) {
+            Debug.Log("Victory!");
+            // TODO: Trigger win condition
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.tag == "Egg") {
+            eggPresent = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision) {
+        if (collision.tag == "Egg") {
+            eggPresent = false;
+        }
+    }
+
+
+}
